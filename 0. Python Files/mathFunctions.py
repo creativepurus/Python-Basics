e137922631b4d3f50466f27417d18759deda8350
Purushottam Anand <124372427+creativepuru@users.noreply.github.com>
2023-04-28 08:30:53 +0530

    Update Python Files

--- 0. Python Files/mathFunctions.py
+++
@@ -1,8 +0,0 @@
-import math
-
-x = 2.9
-print(round(x))
-print(abs(-x))
-
-print(math.ceil(x))
-print(math.floor(x))
--- 0. Python Files/string.py
+++
@@ -1,9 +0,0 @@
-email = '''Hey there
-Welcome in the World of Python.
-
-Thanks,
-The Support Team
-Anand Purushottam
-'''
-
-print(email)
