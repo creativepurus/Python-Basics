2d5adda4e3137e6e955042088984d8c55a6f305d
Purushottam Anand <creativepurus@gmail.com>
2023-06-11 15:38:23 +0530

    💻 Update `Practice examples`

---
+++ 0. Python Files/Strings.py
@@ -0,0 +1,16 @@
+course = 'Python for Beginners'
+
+print("In Upper case : ", course.upper())
+print("Course Name : ", course)
+
+print("In lower case : ", course.lower())
+
+print("Capitalize : ", course.capitalize())
+
+print("Title : ", course.title())
+
+print(course.find('for'))
+
+print(course.replace('for', '4'))
+
+print('Python' in course)
