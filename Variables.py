9870bc7d7200bafa3916b76e3deff4f4eb80d512
Purushottam Anand <creativepurus@gmail.com>
2023-06-11 15:09:56 +0530

    💻 Update `Practice examples`

---
+++ Variables.py
@@ -0,0 +1,23 @@
+print("Hello, world!")
+
+age = 16
+print(age)
+print(type(age))
+
+price = 99.90
+print(price)
+print(type(price))
+
+first_name = "Anand"
+print(first_name)
+
+name = input("What is your name? ")
+print("Hello " + name)
+
+birth_year = int(input("What is your birth year?"))
+age = 2023 - birth_year
+print(age)
+
+birth_year = input("Enter your birth year?")
+age = 2023 - int(birth_year)
+print(age)
