ed0df51ed4b3cffb52a4bd0f9295bbd56a3265ff
Purushottam Anand <creativepurus@gmail.com>
2023-06-13 00:22:47 +0530

    💻 Update `Practice examples`

---
+++ 0. Python Files/11. Lists.py
@@ -0,0 +1,20 @@
+names = ['Anand', 'Puru', "John", 'Mosh', "Pikachu"]
+print(names)
+
+print(names[0])
+
+print(names[-1])
+
+print(names[-2])
+
+print(names[2])
+
+names[2] = "Jon"
+
+print(names)
+
+print(names[0:3])
+
+print(names)
+
+print(names[:3])
