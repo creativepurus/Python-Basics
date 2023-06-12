2e5e72606b2ad82d33f7cfbde90d23e7b1e4011e
Purushottam Anand <creativepurus@gmail.com>
2023-06-13 00:37:08 +0530

    💻 Update `Practice examples`

--- 0. Python Files/12 - List Methods.py
+++ 0. Python Files/12 - List Methods.py
@@ -3,3 +3,17 @@ numbers = [1,2,3,4,5,23,54,346,567]
 numbers.append(100)
 print(numbers)
 
+numbers.insert(2, 200)
+print(numbers)
+
+numbers.remove(567)
+print(numbers)
+
+#numbers.clear()
+print(numbers)
+
+print(1 in numbers)
+
+print(10 in numbers)
+
+print(len(numbers))
