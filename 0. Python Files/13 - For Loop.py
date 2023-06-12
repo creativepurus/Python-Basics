37a1500d0d26b626993961d9fa858ea0bba2a2ac
Purushottam Anand <creativepurus@gmail.com>
2023-06-13 00:47:52 +0530

    💻 Update `Practice examples`

---
+++ 0. Python Files/13 - For Loop.py
@@ -0,0 +1,12 @@
+numbers = [1,2,3,4,5,7]
+
+print("\nUsing For Loop : ")
+for i in numbers:
+    print(i)
+
+
+print("\nUsing While Loop : ")
+i = 0
+while i < len(numbers):
+    print(numbers[i])
+    i += 1
