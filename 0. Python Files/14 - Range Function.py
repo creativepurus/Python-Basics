b627586466b8c0a8c78d29e0e8b7ff469b95fdcf
Purushottam Anand <creativepurus@gmail.com>
2023-06-13 00:52:45 +0530

    💻 Update `Practice examples`

---
+++ 0. Python Files/14 - Range Function.py
@@ -0,0 +1,22 @@
+numbers = range(5)
+print(numbers)
+for number in numbers:
+    print(number)
+
+
+numbers = range(5, 10)
+print(numbers)
+for number in numbers:
+    print(number)
+
+
+numbers = range(0, 10, 2)
+print(numbers)
+for number in numbers:
+    print(number)
+
+
+numbers = range(1, 10, 2)
+print(numbers)
+for number in numbers:
+    print(number)
