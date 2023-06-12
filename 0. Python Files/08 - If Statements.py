d95b64c27ba079f3f32df4b02acd376b26dd1ad1
Purushottam Anand <creativepurus@gmail.com>
2023-06-12 15:56:13 +0530

    💻 Update `Practice examples`

--- 0. Python Files/08 - If Statements.py
+++ 0. Python Files/08 - If Statements.py
@@ -1,7 +1,13 @@
-temperature = 40
+temperature = 2
 
 if temperature > 30:
     print("It's a hot day !")
     print("Drink plenty of water")
+    
+elif temperature > 20:
+    print("It's a nice day !")
+
 else:
     print("It's a cold day")
+
+print("Done !")
