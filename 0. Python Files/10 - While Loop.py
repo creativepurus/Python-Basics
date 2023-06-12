d4c46ef6c3bb4416b1ef1a36f4303ee4be17d628
Purushottam Anand <creativepurus@gmail.com>
2023-06-13 00:14:28 +0530

    💻 Update `Practice examples`

---
+++ 0. Python Files/10 - While Loop.py
@@ -0,0 +1,19 @@
+i = 1
+while i <= 5:
+    print(i)
+    i += 1
+
+j = 1
+while j <= 1_000:
+    print(j)
+    j += 1
+
+i = 1
+while i <= 10:
+    print(i * '*')
+    i += 1
+
+i = 10
+while i >= 1:
+    print(i * '*')
+    i -= 1
