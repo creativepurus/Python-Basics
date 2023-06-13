7a92ff258f2506864c84090f27ee9d30fcba7f0a
Purushottam Anand <creativepurus@gmail.com>
2023-06-13 13:10:37 +0530

    💻 Update `Practice examples`

--- 0. Python Files/17 - Guess Game.py
+++ 0. Python Files/17 - Guess Game.py
@@ -5,3 +5,8 @@ guess_limit = 3
 while guess_count < guess_limit:
     guess = int(input("Guess a number: "))
     guess_count += 1
+    if guess == secret_number:
+        print("You win!")
+        break
+    else:
+        print("")
