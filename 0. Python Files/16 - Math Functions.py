1aa72ad6bcb0dc6a5683d0ace0d358a55169b27e
Purushottam Anand <creativepurus@gmail.com>
2023-06-13 12:51:52 +0530

    💻 Update `Practice examples`

--- 0. Python Files/16 - Math Functions.py
+++ 0. Python Files/16 - Math Functions.py
@@ -1,3 +1,4 @@
+# round Function
 x = 4.2
 print(round(x))
 
@@ -7,3 +8,15 @@ print(round(x))
 x = 4.7
 print(round(x))
 
+# abs Function - It always returns the Positive Number
+print(abs(-10))
+print(abs(-10.3))
+
+# pow Function
+print(pow(2, 3))
+print(pow(2, -3))
+
+import math
+
+print(math.ceil(2.7))
+print(math.floor(2.7))
