6eabc2c5d30164cf6faebac94c001a727ee20733
Purushottam Anand <creativepurus@gmail.com>
2023-06-15 12:16:42 +0530

    💻 Update Practice examples

--- 0. Python Files/13 - For Loop.py
+++ 0. Python Files/13 - For Loop.py
@@ -1,34 +1,49 @@
-# numbers = [1,2,3,4,5,7]
+numbers = [1,2,3,4,5,7]
 
-# print("\nUsing For Loop : ")
-# for i in numbers:
-#     print(i)
+print("\nUsing For Loop : ")
+for i in numbers:
+    print(i)
 
 
-# print("\nUsing While Loop : ")
-# i = 0
-# while i < len(numbers):
-#     print(numbers[i])
-#     i += 1
+print("\nUsing While Loop : ")
+i = 0
+while i < len(numbers):
+    print(numbers[i])
+    i += 1
 
-# print("Using Range function")
-# for i in range(10):
-#     print(i)
+print("Using Range function")
+for i in range(10):
+    print(i)
 
-# prices = [10,20,30]
-# total = 0
-# for price in prices:
-#     total += price
+prices = [10,20,30]
+total = 0
+for price in prices:
+    total += price
 
-# # print(total)
-# print("\nUsing For Loop to calculate the price : ")
-# print(f"Total Price : {total}")
+# print(total)
+print("\nUsing For Loop to calculate the price : ")
+print(f"Total Price : {total}")
 
-# for x in range(4):
-#     for y in range(3):
-#         print(f"X: {x} Y: {y}")
+for x in range(4):
+    for y in range(3):
+        print(f"X: {x} Y: {y}")
 
 numbers = [5,2,5,2,2]
+for num_count in numbers:
+    print(num_count * 'x')
+print()
 
-for num in numbers:
-    print(num * '*')
+numbers = [5,2,5,2,2]
+for num_count in numbers:
+    output = ''
+    for count in range(num_count):
+        output += 'x'
+    print(output)
+print()
+
+numbers = [2,2,2,2,5]
+for num_count in numbers:
+    output = ''
+    for count in range(num_count):
+        output += 'x'
+    print(output)
