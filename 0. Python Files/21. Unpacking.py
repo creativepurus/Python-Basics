8f0fc8fa71c5636ec08fb977566018ed34285e94
Purushottam Anand <creativepurus@gmail.com>
2023-06-15 12:45:16 +0530

    💻 Update Practice examples

---
+++ 0. Python Files/21. Unpacking.py
@@ -0,0 +1,21 @@
+coordinates = (1,2,3)
+
+# x = coordinates[0]
+# y = coordinates[1]
+# z = coordinates[2]
+# print(x, y, z)
+
+# --> Simple way to do the above task is 'Unpacking' which is specially in Python
+
+x, y, z = coordinates
+print(x, y, z)
+
+# --> Unpacking also works with List & Sets along with Tuples
+
+coordinates = [4,5,6]
+x, y, z = coordinates
+print(x, y, z)
+
+coordinates = {7,8,9}
+x, y, z = coordinates
+print(x, y, z)
