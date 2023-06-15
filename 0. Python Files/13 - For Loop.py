87fbeee0d5c19b5fb564b03b072bfcdd00ab71f6
Purushottam Anand <creativepurus@gmail.com>
2023-06-15 12:02:10 +0530

    💻 Update Practice examples

--- 0. Python Files/13 - For Loop.py
+++ 0. Python Files/13 - For Loop.py
@@ -1,17 +1,30 @@
-numbers = [1,2,3,4,5,7]
+# numbers = [1,2,3,4,5,7]
 
-print("\nUsing For Loop : ")
-for i in numbers:
-    print(i)
+# print("\nUsing For Loop : ")
+# for i in numbers:
+#     print(i)
 
 
-print("\nUsing While Loop : ")
-i = 0
-while i < len(numbers):
-    print(numbers[i])
-    i += 1
+# print("\nUsing While Loop : ")
+# i = 0
+# while i < len(numbers):
+#     print(numbers[i])
+#     i += 1
 
-print("Using Range function")
-for i in range(10):
-    print(i)
+# print("Using Range function")
+# for i in range(10):
+#     print(i)
 
+# prices = [10,20,30]
+# total = 0
+# for price in prices:
+#     total += price
+
+# # print(total)
+# print("\nUsing For Loop to calculate the price : ")
+# print(f"Total Price : {total}")
+
+for x in range(4):
+    for y in range(3):
+        print(f"X: {x} Y: {y}")
+        
